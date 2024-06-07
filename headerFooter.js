5b1d93d1a80e155d62839c0eedf0ae1514165d64
Janis Gaucho <botellajvnis@gmail.com>
2024-06-07 03:38:58 +0200

    update

--- headerFooter.js
+++ headerFooter.js
@@ -1,3 +1,4 @@
+// HEADER
 class SpecialHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `        <div class="menu-container">
@@ -5,9 +6,9 @@ class SpecialHeader extends HTMLElement {
                 <p>janis_botella</p>
             </div>
             <div class="menu-links">
-                <p><a href="index.html" class="menu-link">_accueil</a></p>
-                <p><a href="portfolio.html" class="menu-link">_portfolio</a></p>
-                <p><a href="services.html" class="menu-link">_services</a></p>
+                <a href="index.html" class="menu-link active">_accueil</a>
+                <a href="portfolio.html" class="menu-link">_portfolio</a>
+                <a href="services.html" class="menu-link">_services</a>
             </div>
             <div class="menu-contact">
                 <a href="contact.html" class="menu-link">_contactez moi</a>
@@ -15,7 +16,10 @@ class SpecialHeader extends HTMLElement {
          </div>`
     }
 }
+customElements.define('special-header', SpecialHeader)
 
+
+// FOOTER
 class SpecialFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `        <div class="footer-container">
@@ -23,13 +27,13 @@ class SpecialFooter extends HTMLElement {
                 <p>retrouvez moi sur</p>
             </div>
             <div class="socials">
-                <a href="https://x.com/janisgaucho"><i class="fa-brands fa-x-twitter"></i></a>
-                <a href="https://www.linkedin.com/in/janisbotella/"><i class="fa-brands fa-linkedin-in"></i></a>                
-                <a href="https://www.reddit.com/user/janisgaucho/"><i class="fa-brands fa-reddit-alien"></i></a>            
+                <a href="https://x.com/janisgaucho" target="_blank"><i class="fa-brands fa-x-twitter"></i></a>
+                <a href="https://www.linkedin.com/in/janisbotella/" target="_blank"><i class="fa-brands fa-linkedin-in"></i></a>                
+                <a href="https://www.reddit.com/user/janisgaucho/" target="_blank"><i class="fa-brands fa-reddit-alien"></i></a>            
             </div>
 
             <div class="github">
-                <a href="https://github.com/janisgaucho">
+                <a href="https://github.com/janisgaucho" target="_blank">
                     <p>@janisgaucho</p>
                     <i class="fa-brands fa-github"></i></i>
                 </a>
@@ -38,6 +42,4 @@ class SpecialFooter extends HTMLElement {
 `
     }
 }      
-
-customElements.define('special-header', SpecialHeader)
 customElements.define('special-footer', SpecialFooter)
